0c8d75e77bc6f06925eee147c1e4469789e7aafb
ArthurT <arthur.talpaert@isen-lille.fr>
2015-09-30 15:35:04 +0200

    commit init

---
+++ Circle.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   Circle.cpp
+ * Author: isen
+ * 
+ * Created on 30 septembre 2015, 15:15
+ */
+
+#include "Circle.h"
+
+Circle::Circle() {
+}
+
+Circle::Circle(const Circle& orig) {
+}
+
+Circle::~Circle() {
+}
+
+void Circle::setRadius(int radius) {
+
+}
+int Circle::getRadius() {
+
+}
+int Circle::getArea() {
+
+}
+
+
+
+
